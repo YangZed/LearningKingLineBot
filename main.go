d41d7ed0e0a49b5def693066346cc6480c340e81
zed11314 <asd0230zx@gmail.com>
2019-01-10 20:21:54 +0800

    test heroku

--- main.go
+++ main.go
@@ -118,6 +118,7 @@ func httpGet(q string , id string) {
 
     q = strings.Replace( q , " " , "," , -1)    
     //q = strings.Replace("oink oink oink", "oink", "moo", -1)
+    // 140.115.54.66 new ip
     resp, err := http.Get("http://140.115.54.93:8088/?q="+q+"&id="+id)
     if err != nil {
         // handle error
